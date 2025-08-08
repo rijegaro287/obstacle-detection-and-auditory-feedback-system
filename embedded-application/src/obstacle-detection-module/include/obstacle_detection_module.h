b4ccfbd20b1b614426b8682cb7137c8a0527f6ce
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2025-08-08 12:01:24 -0600

    Added color segmentation
    Code migration from Python to C++

--- embedded-application/src/obstacle-detection-module/include/obstacle_detection_module.h
+++ embedded-application/src/obstacle-detection-module/include/obstacle_detection_module.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#ifndef OBSTACLE_DETECTION_MODULE_HPP
+#define OBSTACLE_DETECTION_MODULE_HPP
+
+#include <opencv2/opencv.hpp>
+
+class ObstacleDetectionModule {
+public:
+    ObstacleDetectionModule();
+
+    // Segmenta el color rojo usando los rangos privados
+    cv::Mat segmentRed(const cv::Mat& image) const;
+
+private:
+    // Rangos HSV para rojo (dos rangos para cubrir todo el rojo)
+    cv::Scalar lowerRed1_;
+    cv::Scalar upperRed1_;
+    cv::Scalar lowerRed2_;
+    cv::Scalar upperRed2_;
+};
+
+#endif // OBSTACLE_DETECTION_MODULE_HPP
--- embedded-application/src/obstacle-detection-module/obstacle_detection_module.cpp
+++ embedded-application/src/obstacle-detection-module/obstacle_detection_module.cpp
@@ -2,6 +2,30 @@
 
 #include "obstacle_detection_module.h"
 
+// Contructor: asigna los rangos de rojo para la detección
+ObstacleDetectionModule::ObstacleDetectionModule():
+      // Rango 1 - Tonos (H) del 0 al 10, para todas las saturaciones (S) y brillos (V)
+      lowerRed1_(0, 0, 0),
+      upperRed1_(10, 255, 255),
+      // Rango 2 – Tonos (H) del 160 al 180, para todas las saturaciones (S) y brillos (V)
+      lowerRed2_(160, 0, 0),
+      upperRed2_(180, 255, 255)
+{}
+
+// Método segmentRed: segmenta los rangos de rojo en las imagenes usando una mascara de color
+cv::Mat ObstacleDetectionModule::segmentRed(const cv::Mat& image) const { //image en HSV
+    cv::Mat mask1, mask2, redMask;
+
+    // Crear máscaras para cada rango
+    cv::inRange(image, lowerRed1_, upperRed1_, mask1);
+    cv::inRange(image, lowerRed2_, upperRed2_, mask2);
+
+    // Combinar las dos máscaras
+    cv::bitwise_or(mask1, mask2, redMask);
+
+    return redMask;
+}
+
 int main() {
     std::cout << "Obstacle Detection Module - Starting..." << std::endl;
     
