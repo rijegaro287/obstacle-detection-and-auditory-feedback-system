924cc52f58776a6726d7f6133318ec21b6f118cf
Ricardo Gatgens <gatgens48@gmail.com>
2025-08-29 17:24:04 -0600

    Module init

--- embedded-application/src/configuration-module/configuration_module.cpp
+++ embedded-application/src/configuration-module/configuration_module.cpp
@@ -1,19 +1,17 @@
+#include "configuration_module.hpp"
+#include "configuration_iface.hpp"
+
 #include <iostream>
 
-#include "configuration_module.h"
+ConfigModule& ConfigModule::get_instance() {
+	static ConfigModule instance;
+	return instance;
+}
 
-int main() {
-    std::cout << "Configuration Module - Starting..." << std::endl;
-    
-    // Load system configuration
-    std::cout << "Loading system configuration..." << std::endl;
-    
-    // Initialize configuration parameters
-    std::cout << "Setting up detection parameters..." << std::endl;
-    std::cout << "Setting up audio parameters..." << std::endl;
-    std::cout << "Setting up camera parameters..." << std::endl;
-    
-    std::cout << "Configuration module initialized successfully." << std::endl;
-    
-    return 0;
+ConfigModule::ConfigModule() {
+	// Load configuration settings
+}
+
+void ConfigModule::start() {
+    printf("Starting configuration module...\n");
 }
--- embedded-application/src/main.cpp
+++ embedded-application/src/main.cpp
@@ -1,27 +1,31 @@
 #include "control_module.hpp"
 #include "feedback_module.hpp"
 #include "transmission_module.hpp"
+#include "configuration_module.hpp"
 
 #include <thread>
 #include <pthread.h>
 
 int main() {
-  ControlModule& control_module = ControlModule::get_instance();
-  FeedbackModule& feedback_module = FeedbackModule::get_instance();
-  TransmissionModule& transmission_module = TransmissionModule::get_instance();
+  ConfigModule& config_module = ConfigModule::get_instance();
+  config_module.start();
 
-  printf("Starting Control, Feedback, and Transmission Modules...\n");
-  std::thread control_thread(&ControlModule::start, &control_module);
-  std::thread feedback_thread(&FeedbackModule::start, &feedback_module);
-  std::thread transmission_thread(&TransmissionModule::start, &transmission_module);
+  // ControlModule& control_module = ControlModule::get_instance();
+  // FeedbackModule& feedback_module = FeedbackModule::get_instance();
+  // TransmissionModule& transmission_module = TransmissionModule::get_instance();
 
-  pthread_setschedprio(control_thread.native_handle(), 20);
-  pthread_setschedprio(feedback_thread.native_handle(), 99);
-  pthread_setschedprio(transmission_thread.native_handle(), 20);
+  // printf("Starting Control, Feedback, and Transmission Modules...\n");
+  // std::thread control_thread(&ControlModule::start, &control_module);
+  // std::thread feedback_thread(&FeedbackModule::start, &feedback_module);
+  // std::thread transmission_thread(&TransmissionModule::start, &transmission_module);
 
-  control_thread.join();
-  feedback_thread.join();
-  transmission_thread.join();
+  // pthread_setschedprio(control_thread.native_handle(), 20);
+  // pthread_setschedprio(feedback_thread.native_handle(), 99);
+  // pthread_setschedprio(transmission_thread.native_handle(), 20);
+
+  // control_thread.join();
+  // feedback_thread.join();
+  // transmission_thread.join();
 
   return 0;
 }
