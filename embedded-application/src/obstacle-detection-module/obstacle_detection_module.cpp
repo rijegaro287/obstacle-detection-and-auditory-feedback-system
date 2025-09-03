b0a97a13a58d3bd13b74a17d36b06f98d27a034f
valeriam07 <valeriamorales40@gmail.com>
2025-09-02 21:07:00 -0600

    Added test

--- embedded-application/src/obstacle-detection-module/obstacle_detection_module.cpp
+++ embedded-application/src/obstacle-detection-module/obstacle_detection_module.cpp
@@ -373,39 +373,3 @@ ObstacleDetectionModule::Obstacle ObstacleDetectionModule::startDetection(cv::Ma
 
     return obs;
 }
-
-int main() {
-    ImageCaptureModule capturemod;
-    ObstacleDetectionModule detmod;
-
-    // Inicializar ToF camera
-    if (!capturemod.initialize()) {
-        return -1;
-    }
-
-    // Capturar frames
-    while (true) {
-        // si la captura NO fue exitosa vuelve a intentarlo en la siguiente iteracion/captura
-        if (!capturemod.captureFrame()) {
-            continue;
-        }
-
-        auto [depth, img] = capturemod.preprocessDepth(); // imagen preprocesada y de profundidad
-        if (!img.empty()) {
-            // Visualizar resultado del preprocesamiento
-            detmod.previewDepth(img);
-            
-            // Iniciar deteccion 
-            ObstacleDetectionModule::Obstacle obs;
-            obs = detmod.startDetection(img, depth);  
-            
-            // Visualizar deteccion
-            detmod.viewDetection(obs);
-        }
-
-        int key = cv::waitKey(1);
-        if (key == 27 || key == 'q') break;
-    }
-
-    return 0;
-}
--- embedded-application/src/obstacle-detection-module/test/test_obstacle_detection_module.cpp
+++ embedded-application/src/obstacle-detection-module/test/test_obstacle_detection_module.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 
 #include "obstacle_detection_module.h"
+#include "image_capture_module.h"
 
 int main() {
-  return 0;
+    ImageCaptureModule capturemod;
+    ObstacleDetectionModule detmod;
+
+    // Inicializar ToF camera
+    if (!capturemod.initialize()) {
+        return -1;
+    }
+
+    // Capturar frames
+    while (true) {
+        // si la captura NO fue exitosa vuelve a intentarlo en la siguiente iteracion/captura
+        if (!capturemod.captureFrame()) {
+            continue;
+        }
+
+        auto [depth, img] = capturemod.preprocessDepth(); // imagen preprocesada y de profundidad
+        if (!img.empty()) {
+            // Visualizar resultado del preprocesamiento
+            detmod.previewDepth(img);
+            
+            // Iniciar deteccion 
+            ObstacleDetectionModule::Obstacle obs;
+            obs = detmod.startDetection(img, depth);  
+            
+            // Visualizar deteccion
+            detmod.viewDetection(obs);
+        }
+
+        int key = cv::waitKey(1);
+        if (key == 27 || key == 'q') break;
+    }
+
+    return 0;
 }
